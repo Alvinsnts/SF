366aae109bad0ae675fccd0e768ca7707d372d4b
Alvin Santoso <alvinsantoso0123@gmail.com>
2018-12-14 16:23:21 +0700

    update

--- src/app/addproduct/addproduct.page.ts
+++ src/app/addproduct/addproduct.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NavController } from '@ionic/angular';
+import { SetupService } from '../services/setup.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
 @Component({
   selector: 'app-addproduct',
   templateUrl: './addproduct.page.html',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddproductPage implements OnInit {
 
-  constructor() { }
+  responses: any;
+  productStatus: any;
+  productinfo= {"productname": "", "productprice": "", "producttype": "", "productdetail": ""}
+  constructor(private setupservice: SetupService, private nav: NavController) { }
 
   ngOnInit() {
+  }
+
+  addproduct(){
+
+    console.log(this.productinfo);
+    this.setupservice.postdata('addproduct.php', this.productinfo).subscribe(res =>{
+
+      this.responses = res;
+      console.log(this.responses.status);
+      this.productStatus = this.responses.message;
+      console.log(this.productStatus);
+      console.log(this.responses);
+        this.responses = JSON.stringify(this.responses);
+        localStorage.setItem("productdata", this.responses);
+        this.nav.navigateForward('/home');
+    }, (err: HttpErrorResponse) => 
+    {
+      console.log(err.error);
+      this.responses = err.error;
+      alert(this.responses.message);
+    });
   }
 
 }
